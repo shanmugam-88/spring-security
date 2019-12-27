069f098264d3241adc6535f4a971304dfbd9b660
shanmugam-88 <shanmugamcse.m@gmail.com>
2019-12-27 17:31:47 +0530

    Currency exchange service

---
+++ currency-exchange-service/src/main/java/org/springframework/boot/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,28 @@
+package org.springframework.boot.currencyexchangeservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.currencyexchangeservice.dao.ExchangeValueRepository;
+import org.springframework.boot.currencyexchangeservice.dto.ExchangeValue;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private ExchangeValueRepository exchangeValueRepository;
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from,@PathVariable String to) {
+		int port = Integer.parseInt(environment.getProperty("local.server.port"));
+		ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to); 
+				//new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		exchangeValue.setPort(port);
+		return exchangeValue;		
+	}
+}
