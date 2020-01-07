14b40c9009e1d82397c3a910b14ec3b0433913cc
shanmugam-88 <shanmugamcse.m@gmail.com>
2020-01-07 23:21:18 +0530

    Netflix Eureka Naming and discovery service

---
+++ netflix-eureka-naming-server/src/test/java/org/springframework/boot/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
