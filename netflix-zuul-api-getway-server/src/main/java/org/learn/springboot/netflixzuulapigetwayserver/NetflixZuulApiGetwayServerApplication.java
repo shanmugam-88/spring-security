42467fd6baf0649a8d77365a03063c47740166a0
shanmugam-88 <shanmugamcse.m@gmail.com>
2020-01-07 23:24:33 +0530

    Netflix Zuul API Getway server

---
+++ netflix-zuul-api-getway-server/src/main/java/org/learn/springboot/netflixzuulapigetwayserver/NetflixZuulApiGetwayServerApplication.java
@@ -0,0 +1,25 @@
+package org.learn.springboot.netflixzuulapigetwayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NetflixZuulApiGetwayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGetwayServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
---
+++ netflix-zuul-api-getway-server/src/test/java/org/learn/springboot/netflixzuulapigetwayserver/NetflixZuulApiGetwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.springboot.netflixzuulapigetwayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NetflixZuulApiGetwayServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
