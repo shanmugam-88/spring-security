659e7aa0d8ac29ecee4705623fffa989dc5e8ce6
shanmugam-88 <shanmugamcse.m@gmail.com>
2020-03-30 00:08:19 +0530

    Jwt Implementaion

---
+++ netflix-zuul-api-getway-server/src/main/java/org/learn/springboot/netflixzuulapigetwayserver/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package org.learn.springboot.netflixzuulapigetwayserver.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
